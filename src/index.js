200bcdc3fc140bdca51080cd6001606f1f4dcc4c
WINDOWS-M2UFRC1\eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-02-25 14:02:25 +0200

    Added REACT.js, Bootstap styling

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import CounterReducer from './reducers/counter-reducer';
+
+const Counter = ({
+  value,
+  onInc,
+  onDec
+}) => {
+  return (
+    <div className='col-md-8'>
+      <h1>{value}</h1>
+      <div class="btn-group">
+        <button className="btn btn-secondary" onClick={onInc}>+</button>
+        <button className="btn btn-secondary" onClick={onDec}>-</button>
+      </div>
+    </div>
+  )
+}
+
+// Calling redux library
+const {createStore} = Redux;
+// conste createStore = Redux.createsore;
+const myStore = createStore(CounterReducer);
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+        <div>
+           <Counter
+            value = {myStore.getState()}
+            onInc = {(e) =>{
+              e.preventDefault();
+              console.log(myStore);
+              myStore.dispatch({type: 'INCREMENT'});
+            }}
+            onDec = {() =>{
+              myStore.dispatch({type: 'DECREMENT'});
+            }}
+           />
+        </div>
+    )
+  }
+};
+
+const renderRedux = () => {
+  ReactDOM.render(<App />, document.querySelector('.container-fluid'));
+}
+renderRedux();
+myStore.subscribe(renderRedux);
+
